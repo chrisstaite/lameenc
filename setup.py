f1dc15d30013b25ee1f66d4e2bf43a5401847d69
Chris Staite <chris@yourdreamnet.co.uk>
2023-10-25 20:52:42 +0100

    Update tag.

--- setup.py
+++ setup.py
@@ -66,10 +66,10 @@ if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), '.git
     configuration['setup_requires'] = ['setuptools-git-versioning']
     configuration['setuptools_git_versioning'] = {
         'enabled': True,
-        'starting_version': '1.6.1'
+        'starting_version': '1.6.2'
     }
 else:
-    configuration['version'] = '1.6.1'
+    configuration['version'] = '1.6.2'
 
 # Create the package
 setuptools.setup(**configuration)
