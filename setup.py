b29539824cde9a2ad75e46e1a71a10cc16f18399
Chris Staite <chris@yourdreamnet.co.uk>
2023-10-25 20:41:24 +0100

    Update to Python 3.12.
    Fixes #24

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ Python 3 bindings for the LAME encoding library.
 This library makes it simple to encode PCM data into MP3 without having
 to compile any binaries.
 
-Provides binaries in PyPi for Python 3.7+ for Windows, macOS and Linux.
+Provides binaries in PyPi for Python 3.8+ for Windows, macOS and Linux.
 ''',
     author='Chris Staite',
     author_email='chris@yourdreamnet.co.uk',
