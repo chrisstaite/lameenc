46c8a9a006130b5473e4271f0b3834f6031dbe86
Chris Staite <chris@yourdreamnet.co.uk>
2023-10-25 21:07:26 +0100

    Remove manylinux1 build and add manylinux2.28 build.

--- setup.py
+++ setup.py
@@ -66,10 +66,10 @@ if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), '.git
     configuration['setup_requires'] = ['setuptools-git-versioning']
     configuration['setuptools_git_versioning'] = {
         'enabled': True,
-        'starting_version': '1.6.2'
+        'starting_version': '1.6.3'
     }
 else:
-    configuration['version'] = '1.6.2'
+    configuration['version'] = '1.6.3'
 
 # Create the package
 setuptools.setup(**configuration)
