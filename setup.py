dc96a6e35f6658883daf5aab01f631c846a5d216
Armando Belardo <armandoubelardo@gmail.com>
2023-11-29 18:38:08 -0500

    bump version

--- setup.py
+++ setup.py
@@ -66,10 +66,10 @@ if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), '.git
     configuration['setup_requires'] = ['setuptools-git-versioning']
     configuration['setuptools_git_versioning'] = {
         'enabled': True,
-        'starting_version': '1.6.3'
+        'starting_version': '1.6.4'
     }
 else:
-    configuration['version'] = '1.6.3'
+    configuration['version'] = '1.6.4'
 
 # Create the package
 setuptools.setup(**configuration)
